36a9ff62cffd56492d8561e2189dabf0241e5cf0
hero5907-rgb <hero5907@gmail.com>
2026-01-27 14:34:37 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js (클럽별로 이 파일만 수정하면 됨)
+window.APP_CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/여기에_각클럽_GAS_URL/exec",
+
+  clubName: "동서남북포항라이온스클럽",
+  district: "미아국제라이온스협회 356-E지구",
+
+  slogan: "우리는 봉사로 하나된다",
+  address: "경북 포항시 북구 ○○로 123",
+  phone: "010-1234-5678",
+
+  // (선택) 기본 이미지 (설정시트에 로고/배경이 있으면 그게 우선)
+  logoUrl: "./logo.png",
+  coverUrl: "" // 예: "./cover.jpg" 또는 빈칸
+};
