c911343969ec51f19cb4ce498958d34f970c89ba
hero5907-rgb <hero5907@gmail.com>
2026-02-09 01:41:35 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -942,9 +942,7 @@ if (btnMembersRefresh) {
 
 
   // 로그인 버튼 / 엔터
-  window.addEventListener("DOMContentLoaded", () => {
   el("btnLogin")?.addEventListener("click", handleLogin);
-});
 
 
 // ✅ 상단 이름 터치 → MY 페이지
