5d0b511f08a87a98738e070e2b827f34dfb0e111
hero5907-rgb <hero5907@gmail.com>
2026-06-26 13:14:53 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2485,7 +2485,7 @@ function fitSlogan(el){
 
   if(!el) return;
 
-  let size = 48;      // 최대 글자크기
+  let size = 42;      // 최대 글자크기
   const min = 20;     // 최소 글자크기
 
   el.style.fontSize = size + "px";
